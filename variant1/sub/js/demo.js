66d62140265568a4362c5c7b0ff14bb13eca2c27
Zigmunt <evg.zigmunt@gmail.com>
2014-01-22 14:25:18 +0400

    sub themes, search, colors

---
+++ variant1/sub/js/demo.js
@@ -0,0 +1,24 @@
+$(function () {
+	$('.single').pickmeup({
+		flat	: true
+	});
+	$('.multiple').pickmeup({
+		flat	: true,
+		mode	: 'multiple'
+	});
+	$('.range').pickmeup({
+		flat	: true,
+		mode	: 'range'
+	});
+	var plus_5_days	= new Date;
+	plus_5_days.addDays(0);
+	$('.3-calendars').pickmeup({
+		flat		: true,
+		date		: [
+			new Date,
+			plus_5_days
+		],
+		mode		: 'range',
+		calendars	: 3
+	});
+});
---
+++ variant3/sub/js/script.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+    $(".fontsize").click(function(){                
+        $("body").css("font-size", $(this).attr("proc")+"%");
+    });
+
+    function hide_show_img(what) {
+        $('img').each(function(){            
+            $(this).css("display", (what == 0?"none":"inline") );
+        });
+    }
+
+    $(".sb_change").click(function(){        
+        $("#sv_link").remove();
+        if ($(this).attr("theme") == 'orig') {
+            $("body").css("font-size", "14px");
+            hide_show_img(1);
+        } else {
+            $("body").css("font-size", "150%");
+            $('head').append('<link id="sv_link" href="css/style_' + $(this).attr("theme") + '.css" rel="stylesheet">');
+            hide_show_img(0);
+        }                
+    });
+
+    if ($('#sm_slider').length>0) {
+        $('#sm_slider').smSlider({
+            transition : 'fader',            
+            autoPlay : true,
+            delay : 2500
+        });
+    }
+
+});
